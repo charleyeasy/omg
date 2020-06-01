48ba0369826e506f8c71bc5da7ad0b30924f3059
charleychen <charleychen5405@gmail.com>
2020-06-01 18:53:34 +0800

    complete minus function

--- omg.cpp
+++ omg.cpp
@@ -6,11 +6,16 @@ int add(int x,int y)
 	return x+y;
 }
 
+int minus(int x,int y)
+{
+	return x-y;
+}
 
 int main()
 {
 	int x,y;
 	cin>>x>>y;
 	cout<<add(x,y)<<endl;
+	cout<<minys(x,y)<<endl;
 	return 0; 
 }
