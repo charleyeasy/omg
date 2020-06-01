cd4e515a77f8d99ad27bb276a815172e366d5d70
charleychen <charleychen5405@gmail.com>
2020-06-01 19:30:07 +0800

    complete multify function

--- omg.cpp
+++ omg.cpp
@@ -11,11 +11,17 @@ int minus(int x,int y)
 	return x-y;
 }
 
+int multi(int x,int y)
+{
+	return x*y;
+}
+
 int main()
 {
 	int x,y;
 	cin>>x>>y;
 	cout<<add(x,y)<<endl;
 	cout<<minys(x,y)<<endl;
+	cout<<multi(x,y)<<endl;
 	return 0; 
 }
